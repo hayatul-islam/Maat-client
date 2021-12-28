801b82da77cc9abc98e012a0d855b1c34fa603ae
hayatulemon <hayatulemon340@gmail.com>
2021-12-29 00:21:18 +0600

    add new expetise dashboard

--- src/Pages/Dashboard/AddExpertise/AddExpertise.js
+++ src/Pages/Dashboard/AddExpertise/AddExpertise.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
+import './AddExpertise.css'
 
 const AddExpertise = () => {
+
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => console.log(data);
+
     return (
         <div>
-            <h3>expertise</h3>
+            <div className="big-font py-5">
+                <h2>Add new Expertise</h2>
+            </div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className='inputField addInput'>
+                    <input {...register("title")} placeholder='Title' type='text' />
+                    <input {...register("sub_title")} placeholder='Sub title' type='text' />
+                    <input {...register("image")} placeholder='Image url' />
+                    <textarea {...register("description")} name="" id="" cols="30" rows="3" placeholder='Description'></textarea>
+
+                </div>
+                <div className='pt-4'>
+                    <input className='px-5 py-3 fs-5' type="submit" value='Add Expertise' />
+                </div>
+            </form>
         </div>
     );
 };
