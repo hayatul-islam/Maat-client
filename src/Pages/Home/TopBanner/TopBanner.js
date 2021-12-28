8b3161a394fa30dd1e509d7d55b5a75a7d47b0c9
hayatulemon <hayatulemon340@gmail.com>
2021-12-28 16:11:21 +0600

    top banner responsive

--- src/Pages/Home/TopBanner/TopBanner.js
+++ src/Pages/Home/TopBanner/TopBanner.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import './TopBanner.css';
+// import AOS from 'aos'
+// import 'aos/dist/aos.css';
 
 const TopBanner = () => {
     return (
@@ -8,16 +10,16 @@ const TopBanner = () => {
             <Container>
                 <div className='big-font'>
                     <h1>MULTI-SKILLED</h1>
-                    <h1 className='text-end'>LAW FIRM</h1>
+                    <h1 className='text-end py-4'>LAW FIRM</h1>
                     <h1>OPERATING</h1>
                     <h1>WORLDWIDE</h1>
-                    <h5 className='text-end text-light fw-light'>SINCE 1997</h5>
+                    <h5 className='text-end sinceYear'>SINCE 1997</h5>
                 </div>
                 <div className="mt-4">
                     <a className='downBtn' href="#about"><i className="fas fa-arrow-down"></i></a>
                 </div>
             </Container>
-            <div className='bannerImg'>
+            <div className='bannerImg topBannerImg'>
                 <img src="https://i.ibb.co/bdL6qPm/top-banner.jpg" alt="" />
             </div>
         </div>
