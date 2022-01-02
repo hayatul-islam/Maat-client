5b7cb138095cb1609269512c419d70cdee1f21f5
hayatulemon <hayatulemon340@gmail.com>
2022-01-03 00:30:23 +0600

    banner animation

--- src/Pages/Home/TopBanner/TopBanner.js
+++ src/Pages/Home/TopBanner/TopBanner.js
@@ -7,24 +7,30 @@ const TopBanner = () => {
         <div className='banner-container py-3'>
             <Container>
                 <div className='big-font'>
-                    <h1>MULTI-SKILLED</h1>
-                    <h1 className='text-end py-4'>LAW FIRM</h1>
-                    <h1>OPERATING</h1>
-                    <h1>WORLDWIDE</h1>
-                    <h5 className='text-end sinceYear'>SINCE 1997</h5>
+                    <h1 data-aos="fade-up"
+                        data-aos-duration="1000">MULTI-SKILLED</h1>
+                    <h1 data-aos="fade-up"
+                        data-aos-duration="1000" className='text-end py-4'>LAW FIRM</h1>
+                    <h1 data-aos="fade-up"
+                        data-aos-duration="1000">OPERATING</h1>
+                    <h1 data-aos="fade-up"
+                        data-aos-duration="1000">WORLDWIDE</h1>
+                    <h5 data-aos="fade-up"
+                        data-aos-duration="1000" className='text-end sinceYear'>SINCE 1997</h5>
                 </div>
                 <div className="mt-5 pt-5">
                     <a className='downBtn' href="#about">
                         <i className="fas fa-arrow-down "></i>
                     </a>
                 </div>
+                <div data-aos="fade-right"
+                    data-aos-duration="1000"
+                    data-aos-offset="500"
+                    data-aos-easing="ease-in-sine"
+                    className='bannerImg topBannerImg'>
+                    <img src="https://i.ibb.co/bdL6qPm/top-banner.jpg" alt="" />
+                </div>
             </Container>
-            <div data-aos="fade-right"
-                data-aos-duration="1000"
-                data-aos-easing="ease-in-sine"
-                className='bannerImg topBannerImg'>
-                <img src="https://i.ibb.co/bdL6qPm/top-banner.jpg" alt="" />
-            </div>
         </div>
     );
 };
